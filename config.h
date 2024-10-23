8c4944bfb3aeb015428b6306d9e666bff4ef9880
Aaron John <unrealapex@disroot.org>
2024-10-22 19:31:32 -0700

    feat: rice colorscheme

--- config.h
+++ config.h
@@ -18,8 +18,8 @@ static const float rootcolor[]             = COLOR(0x000000ff);
 static const float fullscreen_bg[]         = {0.1f, 0.1f, 0.1f, 1.0f}; /* You can also use glsl colors */
 static uint32_t colors[][3]                = {
 	/*               fg          bg          border    */
-	[SchemeNorm] = { 0xbbbbbbff, 0x222222ff, 0x444444ff },
-	[SchemeSel]  = { 0xeeeeeeff, 0x005577ff, 0x005577ff },
+	[SchemeNorm] = { 0xbbbbbbff, 0x0d0d0dff, 0x0d0d0dff },
+	[SchemeSel]  = { 0xeeeeeeff, 0xb03060ff, 0xb03060ff },
 	[SchemeUrg]  = { 0,          0,          0x770000ff },
 };
 
